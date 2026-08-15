2aaa7d46789119a79517eb5657ab63446f7effa8
roohi <jexcoht@gmail.com>
2026-08-15 16:11:39 +0330

    22

---
+++ _next/static/FNH4DHjWXIIjN3zWioylA/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcustomerclub\u002Fcustomers\u002F[id]","\u002Fcustomerclub\u002Fvehicles\u002F[id]","\u002Fi\u002F[token]","\u002Fk\u002F[token]","\u002Fproducts\u002F[slug]","\u002Fprofile\u002Forders\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
